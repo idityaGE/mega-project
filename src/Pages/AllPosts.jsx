64d7a3ea671cc1e6f4e8175a89c9763eda8b342a
idity <am44910606@gmail.com>
2024-03-02 02:02:58 +0530

    pages are created

---
+++ src/Pages/AllPosts.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import service from '../appwrite/config/config'
+import Container from '../components/Container'
+import PostCard from '../components/PostCard'
+
+
+function AllPosts() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        service.getPosts([])
+            .then((posts) => {
+                if (posts) {
+                    setPosts(posts.documents)
+                }
+            })
+    }, [])
+    return (
+        <div className='w-full'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
+                    </div>
+                ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default AllPosts
---
+++ src/Pages/EditPost.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import service from '../appwrite/config/config'
+import Container from '../components/Container'
+import PostForm from '../components/post-form/PostForm'
+import { useNavigate, useParams } from 'react-router-dom'
+
+
+function EditPost() {
+    const [post, setPost] = useState([])
+    const { slug } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (slug) {
+            service.getPost(slug)
+                .then((post) => {
+                    if (post) {
+                        setPost(post)
+                    }
+                })
+        } else {
+            navigate('/')
+        }
+    }, [slug, navigate])
+    return post ? (
+        <div className='py-8'>
+            <Container>
+                <PostForm post={post} />
+            </Container>
+        </div>
+    ) : null
+}
+
+export default EditPost
